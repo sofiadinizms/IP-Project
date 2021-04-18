7d9d36eeebb8d1ff2c751e9ee1a2e8f5c029aeb8
JulioBem <phie.souza@gmail.com>
2021-04-18 15:38:42 -0300

    feat(project-grid): define valid spaces

--- #1.py
+++ #1.py
@@ -154,7 +154,16 @@ def convert_shape_format(shape):
         positions[i] = (pos[0] - 2, pos[1] - 4)
 
 def valid_space(shape, grid):
-    pass
+    accepted_pos = [[(j, i) for j in range(10) if grid[i][j] == (0,0,0)] for i in range(20)]
+    accepted_pos = [j for sub in accepted_pos for j in sub]
+
+    formatted = convert_shape_format(shape)
+ 
+    for pos in formatted:
+        if pos not in accepted_pos:
+            if pos[1] > -1:
+                return False
+    return True
 
 def check_lost(positions):
     pass
